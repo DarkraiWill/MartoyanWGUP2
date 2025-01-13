251a472851861b8c695f562d22681c9631a0792e
Will <152715654+DarkraiWill@users.noreply.github.com>
2025-01-13 11:05:12 +0300

    fix

--- MartoyanWGUP2/MainWindow.xaml.cs
+++ MartoyanWGUP2/MainWindow.xaml.cs
@@ -1,24 +1,10 @@
 ﻿using MartoyanWGUP2.Classes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MartoyanWGUP2
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
--- MartoyanWGUP2/Pages/PartnerRealisationHistoryPage.xaml.cs
+++ MartoyanWGUP2/Pages/PartnerRealisationHistoryPage.xaml.cs
@@ -1,23 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MartoyanWGUP2.Pages
 {
-    /// <summary>
-    /// Логика взаимодействия для PartnerRealisationHistoryPage.xaml
-    /// </summary>
     public partial class PartnerRealisationHistoryPage : Page
     {
         public PartnerRealisationHistoryPage(Partner partner)
